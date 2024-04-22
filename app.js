2c63c28b548646076b79810e44ea485524ea8975
NikitaSuharnikov <149718146+NikitaSuharnikov@users.noreply.github.com>
2024-04-22 21:15:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cols = document.querySelectorAll('.col')
 
+//Функция отвечающая за обновления цветов при нажатии на пробел
 document.addEventListener('keydown', event => {
   event.preventDefault()
   if (event.code.toLowerCase() === 'space'){
@@ -7,6 +8,7 @@ document.addEventListener('keydown', event => {
   }
 })
 
+// 
 document.addEventListener('click', event => {
   const type = event.target.dataset.type
   
@@ -15,6 +17,7 @@ document.addEventListener('click', event => {
     ? event.target
     : event.target.children[0]
 
+    //Кусок кода отвечающий за иконку замочка
     node.classList.toggle('fa-lock-open')
     node.classList.toggle('fa-lock')
   } else if (type === 'copy') {
@@ -22,6 +25,7 @@ document.addEventListener('click', event => {
   }
 })
 
+// Функция которая берёт значения заданные в hexCods (все значения шестнадцатиричной системы, т. к. цвета определяются именно с её помощью) и возвращает значение с 6 символами
 function generateRandomCollous() {
   const hexCods = '0123456789ABCDEF'
   let color = ''
@@ -31,11 +35,12 @@ function generateRandomCollous() {
   return '#' + color
 }
 
+// Функция которая при нажатии на номер цвета копирует его
 function copyToCkickboard(text) {
   return navigator.clipboard.writeText(text)
 }
 
-
+// Главная Функция которая передаёт значение generateRandomCollous() в html файл (даёт название цвета заголовку h2 и сам цвет колонке), но только если иконка замочка открыта, если закрыта то просто возвращает пустое значение(цвет колонки и её название не меняется) 
 function setRandomCollours() {
   cols.forEach(col => {
     const isLocked = col.querySelector('i').classList.contains('fa-lock')
@@ -54,7 +59,7 @@ function setRandomCollours() {
    setTextColor(button,color)
   })
 }
-
+// Функция которая пользуется библиотекой chroma для определения цвета текста (если цвет светлый, то текст будет тёмным и наоборот)
 function setTextColor(text, color) {
   const luminance = chroma(color).luminance()
   text.style.color = luminance > 0.5 ? 'black' : 'white'
